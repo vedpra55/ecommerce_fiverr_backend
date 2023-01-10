9dc4094ffb652c2adada7e85cedda348bc6167b3
vedpra55 <vedna400@gmail.com>
2023-01-10 18:29:56 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const apiRoutes = require("./routes/apiRoutes");
 const cookieParser = require("cookie-parser");
 
